83ed533838b35f563427767388f00e10d322a6df
Sean Dooley <sean.dooley.22@gmail.com>
2014-04-21 10:57:30 +0100

    error - lesson 26
    500 internal server error when viewing actor info

--- lesson-26/_partials/header.php
+++ lesson-26/_partials/header.php
@@ -1,11 +1,10 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-	<meta charset=utf-8>
-	<title>Actors</title>
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
-<div class="container">
+    <head>
+        <meta charset="utf-8">
+        <title>Actors</title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="container">
--- lesson-26/js/scripts.js
+++ lesson-26/js/scripts.js
@@ -1,79 +1,81 @@
 var Actors = {
-	init: function( config ) {
-		this.config = config;
+    init: function (config) {
+        this.config = config;
 
-		this.setupTemplates();
-		this.bindEvents();
+        this.setupTemplates();
+        this.bindEvents();
 
-		$.ajaxSetup({
-			url: 'index.php',
-			type: 'POST'
-		});
+        $.ajaxSetup({
+            url: 'index.php',
+            type: 'POST'
+        });
 
-		$('button').remove(); // little lazy
-	},
+        $('button').remove(); // little lazy
+    },
 
-	bindEvents: function() {
-		this.config.letterSelection.on( 'change', this.fetchActors );
-		this.config.actorsList.on( 'click', 'li', this.displayAuthorInfo );
-		this.config.actorInfo.on( 'click', 'span.close', this.closeOverlay );
-	},
+    bindEvents: function () {
+        this.config.letterSelection.on('change', this.fetchActors);
+        this.config.actorsList.on('click', 'li', this.displayAuthorInfo);
+        this.config.actorInfo.on('click', 'span.close', this.closeOverlay);
+    },
 
-	setupTemplates: function() {
-		this.config.actorListTemplate = Handlebars.compile( this.config.actorListTemplate);
-		this.config.actorInfoTemplate = Handlebars.compile( this.config.actorInfoTemplate);
+    setupTemplates: function () {
+        this.config.actorListTemplate = Handlebars.compile(this.config.actorListTemplate);
+        this.config.actorInfoTemplate = Handlebars.compile(this.config.actorInfoTemplate);
 
-		Handlebars.registerHelper( 'fullName', function( actor ) {
-			return actor.first_name + ' ' + actor.last_name;
-		});
-	},
+        Handlebars.registerHelper('fullName', function (actor) {
+            return actor.first_name + ' ' + actor.last_name;
+        });
+    },
 
-	fetchActors: function() {
-		var self = Actors;
+    fetchActors: function () {
+        var self = Actors;
 
-		$.ajax({
-			data: self.config.form.serialize(),
-			dataType: 'json',
-			success: function(results) {
-				self.config.actorsList.empty();
+        $.ajax({
+            data: self.config.form.serialize(),
+            dataType: 'json',
+            success: function (results) {
+                self.config.actorsList.empty();
 
-				if ( results[0] ) {
-					self.config.actorsList.append( self.config.actorListTemplate( results) );
-				} else {
-					self.config.actorsList.append('<li>Nothing returned.</li>');
-				}
-			}
-		});
-	},
+                if (results[0]) {
+                    self.config.actorsList.append(self.config.actorListTemplate(results));
+                } else {
+                    self.config.actorsList.append('<li>Nothing returned.</li>');
+                }
+            }
+        });
+    },
 
-	displayAuthorInfo: function( e ) {
-		var self = Actors;
+    displayAuthorInfo: function (e) {
+        var self = Actors;
 
-		self.config.actorInfo.slideUp( 300 );
+        self.config.actorInfo.slideUp(300);
 
-		$.ajax({
-			data: { actor_id: $(this).data( 'actor_id' ) }
-		}).then(function( results ) {
-			self.config.actorInfo.html( self.config.actorInfoTemplate( { info: results }) ).slideDown(300);
-		});
+        //$.ajax({
+        //	data: { actor_id: $(this).data( 'actor_id' ) }
+        //}).then(function( results ) {
+        //	self.config.actorInfo.html( self.config.actorInfoTemplate( { info: results }) ).slideDown(300);
+        //});
 
-		e.preventDefault();
-	},
+        $.ajax({
+            data: { actor_id: $(this).data('actor_id') }
+        }).done(function (results) {
+            self.config.actorInfo.html(self.config.actorInfoTemplate({ info: results })).slideDown(300);
+        });
 
-	closeOverlay: function() {
-		Actors.config.actorInfo.slideUp(300);
-	}
+        e.preventDefault();
+    },
+
+    closeOverlay: function () {
+        Actors.config.actorInfo.slideUp(300);
+    }
 };
 
 Actors.init({
-	letterSelection: $('#q'),
-	form: $('#actor-selection'),
-	actorListTemplate: $('#actor_list_template').html(),
-	actorInfoTemplate: $('#actor_info_template').html(),
-	actorsList: $('ul.actors_list'),
-	actorInfo: $('div.actor_info')
+    letterSelection: $('#q'),
+    form: $('#actor-selection'),
+    actorListTemplate: $('#actor_list_template').html(),
+    actorInfoTemplate: $('#actor_info_template').html(),
+    actorsList: $('ul.actors_list'),
+    actorInfo: $('div.actor_info')
 });
-
-
-
-
